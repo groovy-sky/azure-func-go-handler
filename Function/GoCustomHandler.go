ca682fb0de7fc2e9fd88e339ee2308e0356ac4f4
JF <github@itgeek.men>
2020-08-27 21:26:03 +0300

    added whatismyip

--- Function/GoCustomHandler.go
+++ Function/GoCustomHandler.go
@@ -3,21 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
-    "time"
     "log"
     "os"
+    "strings"
 )
 
 func httpTriggerHandler(w http.ResponseWriter, r *http.Request) {
-	t := time.Now()
-	fmt.Println(t.Month())
-	fmt.Println(t.Day())
-	fmt.Println(t.Year())
-	ua := r.Header.Get("User-Agent")
-	invocationid := r.Header.Get("X-Azure-Functions-InvocationId")
-	fmt.Printf("Invocationid is: %s \n", invocationid)
-	fmt.Printf("User-Agent is: %s \n", ua)
-    w.Write([]byte(ua))
+	clientIP := r.Header.Get("X-Forwarded-For")
+    w.Write([]byte(strings.Split(clientIP,":")[0]))
 }
 
 
@@ -29,7 +22,6 @@ func main() {
 	}
     mux := http.NewServeMux()
     mux.HandleFunc("/httptrigger", httpTriggerHandler)
-    fmt.Println(mux)
 	log.Println("Go server Listening...on httpInvokerPort:", httpInvokerPort)
 	log.Fatal(http.ListenAndServe(":"+httpInvokerPort, mux))
 }
